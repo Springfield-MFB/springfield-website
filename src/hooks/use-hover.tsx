989c1c4b000a532d4627ee5269826a553a1f2ad4
tobisamcode <samueloluwatobiloba48@gmail.com>
2024-11-30 18:16:06 +0100

    hero page & header 🎉

---
+++ src/hooks/use-hover.tsx
@@ -0,0 +1,25 @@
+import { RefObject, useEffect, useState } from "react";
+
+export const useHover = <T extends HTMLElement = HTMLElement>(
+  ref: RefObject<T>
+) => {
+  const [value, setValue] = useState<boolean>(false);
+
+  const handleMouseOver = () => setValue(true);
+  const handleMouseOut = () => setValue(false);
+
+  useEffect(() => {
+    const node = ref.current;
+    if (node) {
+      node.addEventListener("mouseover", handleMouseOver);
+      node.addEventListener("mouseout", handleMouseOut);
+
+      return () => {
+        node.removeEventListener("mouseover", handleMouseOver);
+        node.addEventListener("mouseout", handleMouseOut);
+      };
+    }
+  }, [ref.current]);
+
+  return [ref, value];
+};
