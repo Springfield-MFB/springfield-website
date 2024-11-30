0b5e6ae89b74c63228cc0578840630de170e6537
tobisamcode <samueloluwatobiloba48@gmail.com>
2024-11-30 18:16:02 +0100

    hero page & header 🎉

---
+++ src/config/index.ts
@@ -0,0 +1,72 @@
+import { MdInfo } from "react-icons/md";
+
+export const NAVBART_ITEMS = [
+  {
+    label: "Company",
+    value: "Company" as const,
+    content: [
+      {
+        icon: MdInfo,
+        iconColor: "#D500C3",
+        href: "#",
+        color: "#FFE8FD",
+        title: "Who we are",
+        desc: "we are a team of passionate and dedicated individuals committed to providing financial services to underserved communities.",
+      },
+
+      {
+        icon: MdInfo,
+        iconColor: "#D500C3",
+        href: "#",
+        color: "#FFE8FD",
+        title: "Who we are",
+        desc: "we are a team of passionate and dedicated individuals committed to providing financial services to underserved communities.",
+      },
+
+      {
+        icon: MdInfo,
+        iconColor: "#D500C3",
+        href: "#",
+        color: "#FFE8FD",
+        title: "Who we are",
+        desc: "we are a team of passionate and dedicated individuals committed to providing financial services to underserved communities.",
+      },
+
+      {
+        icon: MdInfo,
+        iconColor: "#D500C3",
+        href: "#",
+        color: "#FFE8FD",
+        title: "Who we are",
+        desc: "we are a team of passionate and dedicated individuals committed to providing financial services to underserved communities.",
+      },
+
+      {
+        icon: MdInfo,
+        iconColor: "#D500C3",
+        href: "#",
+        color: "#FFE8FD",
+        title: "Who we are",
+        desc: "we are a team of passionate and dedicated individuals committed to providing financial services to underserved communities.",
+      },
+
+      {
+        icon: MdInfo,
+        iconColor: "#D500C3",
+        href: "#",
+        color: "#FFE8FD",
+        title: "Who we are",
+        desc: "we are a team of passionate and dedicated individuals committed to providing financial services to underserved communities.",
+      },
+
+      {
+        icon: MdInfo,
+        iconColor: "#D500C3",
+        href: "#",
+        color: "#FFE8FD",
+        title: "Who we are",
+        desc: "we are a team of passionate and dedicated individuals committed to providing financial services to underserved communities.",
+      },
+    ],
+  },
+];
