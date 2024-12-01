91ed3a3f82ae7e5a4e69dd8eb1165f32bd62ef99
Tobi Adesokan <70437868+tobisamcode@users.noreply.github.com>
2024-12-01 19:07:09 +0100

    hero-page done 🎉

--- src/components/max-width-wrapper.tsx
+++ src/components/max-width-wrapper.tsx
@@ -13,7 +13,7 @@ export const MaxWidthWrapper = ({
     <div>
       <div
         className={cn(
-          "h-full mx-auto w-full max-w-screen-2xl px-2.5 md:px-20",
+          "h-full mx-auto w-full max-w-screen-xl px-2.5 md:px-20",
           className
         )}
       >
