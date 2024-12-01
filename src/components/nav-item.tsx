c06dc6c1090231f9bb65ae64b4a3681c59aec533
tobisamcode <samueloluwatobiloba48@gmail.com>
2024-12-01 10:06:29 +0100

    hero page & header 🎉

--- src/components/nav-item.tsx
+++ src/components/nav-item.tsx
@@ -3,28 +3,29 @@
 import { NAVBART_ITEMS } from "@/config";
 import { cn } from "@/lib/utils";
 import { ChevronDown } from "lucide-react";
-import { FC } from "react";
+import { FC, RefObject } from "react";
 
 type NavItem = (typeof NAVBART_ITEMS)[number];
 
 interface NavItemProps {
   navItem: NavItem;
-  handleopen: () => void;
   isOpen: boolean;
   isAnyOpen: boolean;
+  onMouseEnter: () => void;
 }
 
 export const NavItem: FC<NavItemProps> = ({
-  handleopen,
   isOpen,
   isAnyOpen,
   navItem,
+  onMouseEnter,
 }) => {
   return (
     <div className="flex">
       <div
-        onClick={handleopen}
-        className="relative flex gap-x-1 items-center text-dark-primary text-sm cursor-pointer"
+        className="relative flex gap-x-1 items-center text-dark-primary text-sm cursor-pointer
+      "
+        onMouseEnter={onMouseEnter}
       >
         <span>{navItem.label}</span>
         <ChevronDown
@@ -39,15 +40,21 @@ export const NavItem: FC<NavItemProps> = ({
         <div
           className={cn(
             "absolute inset-x-0 top-full text-sm animate-slide-down",
-            isOpen ? "animate-slide-down" : "animate-slide-up"
+            isOpen && "animate-slide-down",
+            {
+              "animate-in  fade-in-10 slide-in-from-top-16": !isAnyOpen,
+            }
           )}
         >
           <div className="relative  bg-white">
             <div className="mx-auto w-full">
-              <div className="grid grid-cols-3 gap-x-8 gap-y-10 pt-8">
+              <div className="grid grid-cols-3 gap-x-8 gap-y-10 pt-4 pb-8">
                 <div className="col-span-4 col-start-1 grid  grid-cols-3">
                   {navItem.content.map((item, i) => (
-                    <div key={i} className="flex rounded-lg  bg-white p-4">
+                    <div
+                      key={item.title}
+                      className="flex rounded-lg  bg-white p-4"
+                    >
                       <div className="flex gap-x-2 ">
                         <div
                           style={{ backgroundColor: item.color }}
