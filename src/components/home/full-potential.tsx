0341dd07b7764b877890793d676071afdb58adee
Tobi Adesokan <70437868+tobisamcode@users.noreply.github.com>
2024-12-02 00:56:13 +0100

    hero-page done 🎉

--- src/components/home/full-potential.tsx
+++ src/components/home/full-potential.tsx
@@ -1,24 +1,19 @@
 import React, { useState } from "react";
 import { Heading } from "../heading";
 import { SERVICES } from "@/config";
+import { PotentialCarousel } from "./full-potential-carousel";
 
 export const FullPotential = () => {
-  const [currentSlide, setCurrentSlide] = useState(0);
-
-  const handlePrev = () => {
-    setCurrentSlide((prev) => (prev === 0 ? SERVICES.length - 1 : prev - 1));
-  };
-
-  const handleNext = () => {
-    setCurrentSlide((prev) => (prev === SERVICES.length - 1 ? 0 : prev + 1));
-  };
-
   return (
-    <div className="w-full">
+    <div className="w-full pt-16">
       <Heading className=" lg:w-[70%]">
         Unleash the <span className="text-brand-primary">Full Potential</span>{" "}
         of Your Business
       </Heading>
+
+      <div className="full">
+        <PotentialCarousel />
+      </div>
     </div>
   );
 };
