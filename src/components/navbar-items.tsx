a896e52c56629675815c65be59aec44272404b45
tobisamcode <samueloluwatobiloba48@gmail.com>
2024-12-01 10:06:33 +0100

    hero page & header 🎉

--- src/components/navbar-items.tsx
+++ src/components/navbar-items.tsx
@@ -1,32 +1,20 @@
 "use client";
 
 import { NAVBART_ITEMS } from "@/config";
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import { NavItem } from "./nav-item";
+import { useHover } from "@/hooks/use-hover";
 import { useOnClickOutside } from "@/hooks/use-on-click-outside";
 
 export const NavbarItems = () => {
-  const [activeIndex, setActiveIndex] = useState<number | null>(null);
-
-  useEffect(() => {
-    const handler = (event: KeyboardEvent) => {
-      if (event.key === "Escape") {
-        setActiveIndex(null);
-      }
-    };
-
-    document.addEventListener("keydown", handler);
-
-    return () => {
-      document.removeEventListener("keydown", handler);
-    };
-  }, [activeIndex]);
-
-  const isAnyOpen = activeIndex !== null;
-
+  const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
   const navRef = useRef<HTMLDivElement | null>(null);
 
-  useOnClickOutside(navRef, () => setActiveIndex(null));
+  const isAnyOpen = hoveredIndex !== null;
+
+  useOnClickOutside(navRef, () => setHoveredIndex(null));
+
+  const [ref, value] = useHover(navRef);
 
   return (
     <div
@@ -34,22 +22,14 @@ export const NavbarItems = () => {
       ref={navRef}
     >
       {NAVBART_ITEMS.map((item, i) => {
-        const handleOpen = () => {
-          if (activeIndex === i) {
-            setActiveIndex(null);
-          } else {
-            setActiveIndex(i);
-          }
-        };
-
-        const isOpen = activeIndex === i;
+        const isHovered = value ? hoveredIndex === i : false;
 
         return (
           <NavItem
+            onMouseEnter={() => setHoveredIndex(i)}
             key={i}
             navItem={item}
-            handleopen={handleOpen}
-            isOpen={isOpen}
+            isOpen={isHovered}
             isAnyOpen={isAnyOpen}
           />
         );
