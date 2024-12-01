41d64ece0c27c1bc9f0ffc5da74e45c2d959aa8a
Tobi Adesokan <70437868+tobisamcode@users.noreply.github.com>
2024-12-01 19:07:15 +0100

    hero-page done 🎉

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,4 +1,4 @@
-import { ChevronDown } from "lucide-react";
+"use client";
 
 import Link from "next/link";
 import Image from "next/image";
@@ -8,19 +8,23 @@ import Logo from "../../public/svg/logo.svg";
 import { MaxWidthWrapper } from "./max-width-wrapper";
 import { Button } from "./ui/button";
 import { NavbarItems } from "./navbar-items";
+import { useState } from "react";
+import { AnimatedHamburgerButton } from "./hamburger-button";
 
 export const Navbar = () => {
+  const [active, setActive] = useState(false);
+
   return (
-    <div className="sticky z-[100] h-24 inset-x-0 top-0 bg-white w-full shadow-navbar backdrop-blur-lg transition-all ">
+    <div className="sticky z-[100] h-20 lg:h-20 inset-x-0 top-0 bg-white w-full shadow-navbar backdrop-blur-lg transition-all ">
       <MaxWidthWrapper>
-        <div className="relative flex h-24  items-center justify-between">
+        <div className="relative flex h-20 items-center justify-between">
           {/* Logo */}
 
           <Link href="/">
             <Image
               src={Logo}
               alt="Logo"
-              className=" sm:w-16 sm:h-12 lg:w-48 lg:h-14"
+              className="w-36 h-auto lg:w-40 lg:h-14"
               width={1000}
               height={1000}
             />
@@ -31,35 +35,11 @@ export const Navbar = () => {
             <NavbarItems />
           </div>
 
-          {/* <div className="hiden md:flex items-center space-x-6">
-            <div className="flex gap-x-1 items-center text-dark-primary text-sm cursor-pointer">
-              <span>Company</span>
-              <ChevronDown size={18} className="text-dark-primary" />
-            </div>
-
-            <div className="flex gap-x-1 items-center text-dark-primary text-sm cursor-pointer">
-              <span>Savings & Deposit</span>
-              <ChevronDown size={18} className="text-dark-primary" />
-            </div>
-
-            <div className="flex gap-x-1 items-center text-dark-primary text-sm cursor-pointer">
-              <span>Loans</span>
-              <ChevronDown size={18} className="text-dark-primary" />
-            </div>
-
-            <div className="flex gap-x-1 items-center text-dark-primary text-sm cursor-pointer">
-              <span>Digital Services</span>
-              <ChevronDown size={18} className="text-dark-primary" />
-            </div>
-
-            <div className="flex gap-x-1 items-center text-dark-primary text-sm cursor-pointer">
-              <span>Contact Us</span>
-              <ChevronDown size={18} className="text-dark-primary" />
-            </div>
-          </div> */}
-
           {/* Buttons */}
-          <Button>Internet Banking</Button>
+          <Button className="hidden lg:block">Internet Banking</Button>
+          <div className="flex lg:hidden">
+            <AnimatedHamburgerButton active={active} setActive={setActive} />
+          </div>
         </div>
       </MaxWidthWrapper>
     </div>
