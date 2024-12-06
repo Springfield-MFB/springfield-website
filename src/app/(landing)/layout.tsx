82b1db43a2d6dff669ea444956d064d98b74851a
Tobi Adesokan <70437868+tobisamcode@users.noreply.github.com>
2024-12-06 01:12:37 +0100

    landing-page done 🎉

--- src/app/(landing)/layout.tsx
+++ src/app/(landing)/layout.tsx
@@ -1,3 +1,4 @@
+import { Footer } from "@/components/footer";
 import { Navbar } from "@/components/navbar";
 import { FC, ReactNode } from "react";
 
@@ -6,6 +7,7 @@ const Layout: FC<{ children: ReactNode }> = ({ children }) => {
     <>
       <Navbar />
       {children}
+      <Footer />
     </>
   );
 };
