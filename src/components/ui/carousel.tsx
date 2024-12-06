693a1114df233437ef2d8cc425db23c58bba8eea
Tobi Adesokan <70437868+tobisamcode@users.noreply.github.com>
2024-12-06 01:12:54 +0100

    landing-page done 🎉

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -210,7 +210,7 @@ const CarouselPrevious = React.forwardRef<
           ? "-left-12 top-1/2 -translate-y-1/2"
           : "-top-12 left-1/2 -translate-x-1/2 rotate-90",
         {
-          "border-black": canScrollPrev,
+          "border-2 border-black": canScrollPrev,
         },
         className
       )}
@@ -242,7 +242,7 @@ const CarouselNext = React.forwardRef<
           ? "-right-12 top-1/2 -translate-y-1/2"
           : "-bottom-12 left-1/2 -translate-x-1/2 rotate-90",
         {
-          "border-black": canScrollNext,
+          "border-2 border-black": canScrollNext,
         },
         className
       )}
