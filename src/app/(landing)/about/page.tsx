0415829e567ad9cbfa6d08e3d76fed71aac48c63
Tobi Adesokan <70437868+tobisamcode@users.noreply.github.com>
2024-12-07 19:02:01 +0100

    flip-text animation

---
+++ src/app/(landing)/about/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { AboutPage } from "./comoponents/about-page";
+
+const Page = () => {
+  return (
+    <div>
+      <AboutPage />
+    </div>
+  );
+};
+
+export default Page;
