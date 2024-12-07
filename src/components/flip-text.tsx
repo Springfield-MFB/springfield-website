e5cd74b299f9c47c5777693db59dda598ac8eaa9
Tobi Adesokan <70437868+tobisamcode@users.noreply.github.com>
2024-12-07 19:02:20 +0100

    flip-text animation

--- src/components/flip-text.tsx
+++ src/components/flip-text.tsx
@@ -36,3 +36,66 @@ export const FlipText = ({
     </motion.div>
   );
 };
+
+const DURATION = 0.25;
+const STAGGER = 0.025;
+
+export const StaggeredFlipText = ({
+  children,
+  className,
+}: {
+  children: string;
+  className?: string;
+}) => {
+  return (
+    <motion.div
+      initial="initial"
+      whileHover="hovered"
+      className={cn("relative overflow-hidden whitespace-nowrap", className)}
+    >
+      <div>
+        {children.split("").map((l, i) => {
+          return (
+            <motion.span
+              variants={{
+                initial: { y: 0 },
+                hovered: { y: "-100%" },
+              }}
+              transition={{
+                duration: DURATION,
+                ease: "easeInOut",
+                delay: i * STAGGER,
+              }}
+              key={i}
+              className="inline-block"
+            >
+              {l}
+            </motion.span>
+          );
+        })}
+      </div>
+
+      <div className="absolute inset-0">
+        {children.split("").map((l, i) => {
+          return (
+            <motion.span
+              variants={{
+                initial: { y: "100%" },
+                hovered: { y: "0" },
+              }}
+              transition={{
+                duration: DURATION,
+                ease: "easeInOut",
+                delay: i * STAGGER,
+              }}
+              key={i}
+              className="inline-block"
+            >
+              {l}
+            </motion.span>
+          );
+        })}
+      </div>
+    </motion.div>
+  );
+};
