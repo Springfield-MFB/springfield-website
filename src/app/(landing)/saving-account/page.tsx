05c03185795a5b40b3d53104b78359532b8ab538
Tobi Adesokan <70437868+tobisamcode@users.noreply.github.com>
2024-12-09 01:46:52 +0100

    flip-text animation

---
+++ src/app/(landing)/saving-account/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { SavingPage } from "./components/savings-page";
+
+const Page = () => {
+  return (
+    <div>
+      <SavingPage />
+    </div>
+  );
+};
+
+export default Page;
