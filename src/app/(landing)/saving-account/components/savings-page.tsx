139524bb953a68cb0cd1fb5a2ce151ab1ef78350
Tobi Adesokan <70437868+tobisamcode@users.noreply.github.com>
2024-12-09 01:47:13 +0100

    flip-text animation

---
+++ src/app/(landing)/saving-account/components/savings-page.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import { SavingHero } from "./saving-hero";
+import { DocumentRequired } from "./document-required";
+import { Benefit } from "./benefit";
+
+export const SavingPage = () => {
+  return (
+    <>
+      <div className="relative">
+        {/* Hero section */}
+        <SavingHero />
+
+        {/* Optional: Background grid effect */}
+        <div className="absolute top-[40%] -z-20 opacity-70  left-0 right-0 w-full ">
+          <Image
+            src="/images/building.png"
+            alt="Building"
+            width={1000}
+            height={1000}
+            className="h-full w-full object-cover"
+          />
+        </div>
+      </div>
+
+      {/* Document Required */}
+      <div className="relative py-12 lg:py-24">
+        <div className="absolute inset-x-0 inset-y-0  bg-dark-primary -z-10" />
+        <DocumentRequired />
+      </div>
+
+      {/* Benefit of Savings */}
+      <Benefit />
+    </>
+  );
+};
