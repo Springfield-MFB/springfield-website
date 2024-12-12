099320bc88c51602af30df231dc9ba3d82d62f0e
Tobi Adesokan <70437868+tobisamcode@users.noreply.github.com>
2024-12-12 19:42:03 +0100

    career, mgt team, contact us, faq, whistle-blowing 🟡 - in progress

---
+++ src/components/page-board.tsx
@@ -0,0 +1,33 @@
+import { Heading } from "./heading";
+import { MaxWidthWrapper } from "./max-width-wrapper";
+
+interface PageBoardProps {
+  backgroundImage: string;
+  desc: string;
+  heading: string;
+}
+
+export const PageBoard: React.FC<PageBoardProps> = ({
+  backgroundImage,
+  desc,
+  heading,
+}) => {
+  return (
+    <div
+      className="relative flex justify-center items-center h-[450px] bg-black bg-cover text-white py-16 mb-16"
+      style={{
+        backgroundImage: `${backgroundImage}`, // Replace with the actual image path
+        backgroundPosition: "top 40% center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      {/* Overlay */}
+      <div className="absolute inset-0 bg-black/80"></div>
+
+      <MaxWidthWrapper className="relative text-center lg:w-[80%] mx-auto">
+        <Heading className="text-white">{heading}</Heading>
+        <p className="text-xs mt-3 leading-6">{desc}</p>
+      </MaxWidthWrapper>
+    </div>
+  );
+};
