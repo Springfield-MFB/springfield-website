bbfbc36589d59d7cd57823ca6247d466242f7957
Tobi Adesokan <70437868+tobisamcode@users.noreply.github.com>
2024-12-12 19:41:41 +0100

    career, mgt team, contact us, faq, whistle-blowing 🟡 - in progress

---
+++ src/app/(landing)/contact-us/components/contact-us-page.tsx
@@ -0,0 +1,13 @@
+import { PageBoard } from "@/components/page-board";
+
+export const ContactUsPage = () => {
+  return (
+    <div>
+      <PageBoard
+        backgroundImage="url('/images/help-bg.jpeg')"
+        heading="How can we help?"
+        desc="Looking for the solution that best fits your business needs? Fill out the contact form and our Sales team will be in touch."
+      />
+    </div>
+  );
+};
