c357421ab026af1e83151dde5c6634c056eadacf
Tobi Adesokan <70437868+tobisamcode@users.noreply.github.com>
2024-12-20 06:00:09 +0100

    career, mgt team, contact us, faq, whistle-blowing 🟡 - in progress

---
+++ src/app/(landing)/deposit-calculator/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { DepositCalculator } from "./deposit-calculator";
+
+const Page = () => {
+  return (
+    <div className="pt-16 pb-40">
+      <DepositCalculator mode="light" />
+    </div>
+  );
+};
+
+export default Page;
