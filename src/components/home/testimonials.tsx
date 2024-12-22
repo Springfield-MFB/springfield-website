316c47e4be58f2c0dd33822c198089ebcae651dc
Tobi Adesokan <70437868+tobisamcode@users.noreply.github.com>
2024-12-22 02:03:56 +0100

    animations 🎉

--- src/components/home/testimonials.tsx
+++ src/components/home/testimonials.tsx
@@ -1,13 +1,17 @@
 import { Heading } from "../heading";
+import BoxReveal from "../ui/box-reveal";
 import { TestimonialCarousel } from "./testimonial-carousel";
 
 export const Testimonials = () => {
   return (
     <div>
       <div className="w-[53%]">
-        <Heading>
-          What are <span className="text-brand-primary">customers</span> saying?
-        </Heading>
+        <BoxReveal boxColor={"#F0B929"} duration={1}>
+          <Heading>
+            What are <span className="text-brand-primary">customers</span>{" "}
+            saying?
+          </Heading>
+        </BoxReveal>
       </div>
 
       <div className="w-full mt-8">
