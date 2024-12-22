dba5d7924b96e0167aad3f759d5a95b0dbc55826
Tobi Adesokan <70437868+tobisamcode@users.noreply.github.com>
2024-12-22 02:03:35 +0100

    animations 🎉

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,21 +1,72 @@
 "use client";
 
+import { useEffect, useRef, useState } from "react";
 import Link from "next/link";
-import Image from "next/image";
 
-import Logo from "@/public/svg/logo.svg";
+import Image from "next/image";
 
 import { MaxWidthWrapper } from "./max-width-wrapper";
 import { Button } from "./ui/button";
 import { NavbarItems } from "./navbar-items";
-import { useState } from "react";
+import { NAVBART_ITEMS } from "@/config";
+
 import { AnimatedHamburgerButton } from "./hamburger-button";
+import Logo from "@/public/svg/logo.svg";
+import AccordionItem from "./mobile-item";
+import { cn } from "@/lib/utils";
 
 export const Navbar = () => {
   const [active, setActive] = useState(false);
+  const [openItem, setOpenItem] = useState<string | null>(null);
+
+  const handleAccordionClick = (item: string) => {
+    setOpenItem(openItem === item ? null : item);
+  };
+
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (active) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [active]);
 
   return (
-    <div className="sticky z-[100] h-20 lg:h-20 inset-x-0 top-0 bg-white w-full shadow-navbar backdrop-blur-lg transition-all ">
+    <div className="sticky z-[100] lg:h-20 inset-x-0 top-0 bg-white w-full shadow-navbar backdrop-blur-lg transition-all ">
+      {/* MOBILE MENU */}
+      <div
+        className={`absolute top-[78px] inset-0 z-10 bg-white transition-all duration-500 ease-in-out`}
+        style={{
+          height: active ? `${menuRef.current?.scrollHeight}px` : "0px",
+          opacity: active ? 1 : 0,
+        }}
+        ref={menuRef}
+      >
+        <nav
+          className={cn(
+            "flex flex-col space-y-4 overflow-auto pt-10 pb-32 px-4",
+            active ? "h-[100vh] translate-y-0" : "translate-y-[100%] bg-black"
+          )}
+        >
+          {NAVBART_ITEMS.map((item) => (
+            <AccordionItem
+              key={item.label}
+              item={item}
+              isOpen={openItem == item.label}
+              close={() => setActive(false)}
+              onClick={() => handleAccordionClick(item.label)}
+            >
+              {item.label}
+            </AccordionItem>
+          ))}
+          <Button className="w-[90%] py-6 mx-auto ">Internet Banking</Button>
+        </nav>
+      </div>
       <MaxWidthWrapper>
         <div className="relative flex h-20 items-center justify-between">
           {/* Logo */}
@@ -24,7 +75,7 @@ export const Navbar = () => {
             <Image
               src={Logo}
               alt="Logo"
-              className="w-36 h-auto lg:w-40 lg:h-14"
+              className="w-36 h-auto lg:w-40 lg:h-14 z-40"
               width={1000}
               height={1000}
             />
