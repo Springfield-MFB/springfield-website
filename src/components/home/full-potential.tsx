7b20225058e610aaa36c7097a5057b70b48a41e6
Tobi Adesokan <70437868+tobisamcode@users.noreply.github.com>
2024-12-22 02:03:44 +0100

    animations 🎉

--- src/components/home/full-potential.tsx
+++ src/components/home/full-potential.tsx
@@ -2,14 +2,17 @@ import React, { useState } from "react";
 import { Heading } from "../heading";
 import { SERVICES } from "@/config";
 import { PotentialCarousel } from "./full-potential-carousel";
+import BoxReveal from "../ui/box-reveal";
 
 export const FullPotential = () => {
   return (
     <div className="w-full pt-16">
-      <Heading className=" lg:w-[70%]">
-        Unleash the <span className="text-brand-primary">Full Potential</span>{" "}
-        of Your Business
-      </Heading>
+      <BoxReveal boxColor={"#F0B929"} duration={1.2}>
+        <Heading className=" lg:w-[70%]">
+          Unleash the <span className="text-brand-primary">Full Potential</span>{" "}
+          of Your Business
+        </Heading>
+      </BoxReveal>
 
       <div className="full">
         <PotentialCarousel />
