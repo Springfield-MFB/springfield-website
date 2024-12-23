6c4ee36915ddd687729d5161623f54401a05186b
Tobi Adesokan <70437868+tobisamcode@users.noreply.github.com>
2024-12-23 10:33:46 +0100

    latext update - animation 🎉

--- src/app/(landing)/micro-loan/page.tsx
+++ src/app/(landing)/micro-loan/page.tsx
@@ -1,4 +1,8 @@
-"use client";
+export const metadata = {
+  title: "Micro Loan",
+  description:
+    "Specialized loan product thoughtfully designed to provide financial support and empower small and micro business owners to grow and sustain their ventures.",
+};
 
 import { MicroLoanPage } from "./components/micro-loan-page";
 
