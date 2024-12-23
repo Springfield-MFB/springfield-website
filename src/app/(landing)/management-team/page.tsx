36d4350333629b9da6c6b4de9b7d8dbd920243e6
Tobi Adesokan <70437868+tobisamcode@users.noreply.github.com>
2024-12-23 10:33:59 +0100

    latext update - animation 🎉

--- src/app/(landing)/management-team/page.tsx
+++ src/app/(landing)/management-team/page.tsx
@@ -1,5 +1,11 @@
 import { ManagementTeamPage } from "./components/management-team";
 
+export const metadata = {
+  title: "Management Team",
+  description:
+    "Our management team is committed to leading by example, fostering a culture of innovation, and driving excellence in everything we do. ",
+};
+
 const Page = () => {
   return <ManagementTeamPage />;
 };
