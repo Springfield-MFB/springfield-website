04034ed35b2eadaef7289e1e421a94872813e9b5
Tobi Adesokan <70437868+tobisamcode@users.noreply.github.com>
2024-12-23 10:33:42 +0100

    latext update - animation 🎉

--- src/app/(landing)/asset-acquisition-loan/page.tsx
+++ src/app/(landing)/asset-acquisition-loan/page.tsx
@@ -1,6 +1,10 @@
-"use client";
-
 import { AssetAcquisitionLoan } from "./components/asset-acquisition-loan";
+
+export const metadata = {
+  title: "Asset Acquisition Loan",
+  description:
+    "This is a product designed to give our customers access to funding asset acquisition like Home appliances, electronics and phones etc.",
+};
 
 const Page = () => {
   return <AssetAcquisitionLoan />;
