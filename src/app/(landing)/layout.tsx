a4233eb25ffd81e8776a0e9780c1cc2e905a90aa
Tobi Adesokan <70437868+tobisamcode@users.noreply.github.com>
2024-12-24 06:28:35 +0100

    new fixes 🎉

--- src/app/(landing)/layout.tsx
+++ src/app/(landing)/layout.tsx
@@ -6,16 +6,19 @@ import Image from "next/image";
 
 import CbnLogo from "@/public/svg/cbn-logo.svg";
 import NdicLogo from "@/public/svg/ndic-logo.svg";
+import TawkToChat from "@/components/chatbot/tawk-to-chat";
+import { MaxWidthWrapper } from "@/components/max-width-wrapper";
 
 const Layout: FC<{ children: ReactNode }> = ({ children }) => {
   return (
     <div className="relative">
       <Navbar />
       {children}
+      <TawkToChat />
       <Footer />
 
-      <div className="fixed -bottom-6 left-4">
-        <div className="bg-white/65 p-2 rounded-md text-center lg:text-left mb-8 flex justify-center lg:justify-start">
+      <div className="fixed -bottom-6 left-2 lg:left-20 z-50">
+        <div className="bg-white/40 backdrop-blur-sm p-2 rounded-md text-center lg:text-left mb-8 flex justify-center lg:justify-start">
           <div className="text-sm font-normal flex gap-x-2 items-center">
             <span className="">Licensed by CBN</span>
             <Image
@@ -28,7 +31,7 @@ const Layout: FC<{ children: ReactNode }> = ({ children }) => {
             <span className="">Insured by NDIC</span>
             <Image
               src={NdicLogo}
-              alt="CBN Logo"
+              alt="NDIC Logo"
               width={1000}
               height={1000}
               className="size-[40px]"
