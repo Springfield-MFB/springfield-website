e7bb1ba1adf7fdff518ea9c94dedc56f08e18c84
Tobi Adesokan <70437868+tobisamcode@users.noreply.github.com>
2024-12-31 00:35:02 +0100

    new fixes 🎉

--- src/app/(landing)/careers/components/openings.tsx
+++ src/app/(landing)/careers/components/openings.tsx
@@ -4,39 +4,43 @@ import Image from "next/image";
 
 export const Openings = () => {
   return (
-    <MaxWidthWrapper>
-      <div className="flex flex-col py-16">
-        <Heading className="text-center">
-          Current Job Opening at Springfield Microfinance Bank
-        </Heading>
-        <p className="mt-4 text-sm text-center">
-          Here are some of our available openings
-        </p>
+    <div id="openings">
+      <MaxWidthWrapper>
+        <div className="flex flex-col py-16">
+          <Heading className="text-center">
+            Current Job Opening at Springfield Microfinance Bank
+          </Heading>
+          <p className="mt-4 text-sm text-center">
+            Here are some of our available openings
+          </p>
 
-        <div className="flex flex-col space-y-4 lg:w-[75%] lg:mx-auto mt-16">
-          {["Manager", "Accountant", "Human Resource", "Security"].map(
-            (item, index) => (
-              <div
-                key={index}
-                className="flex items-center rounded-[8px] border-[.75px] border-[#D6D6D6] shadow-sm py-2 text-xs text-center"
-              >
-                <span className="w-[30%] border-r py-1 font-bold">{item}</span>
-                <span className="w-[40%] flex justify-center items-center space-x-2 border-r py-1">
-                  <Image
-                    src="/svg/flag.svg"
-                    alt="photo flag"
-                    width={20}
-                    height={20}
-                    className="rounded-full "
-                  />
-                  <p>Lagos - Nigeria</p>
-                </span>
-                <span className="w-[30%] py-1">Full-time</span>
-              </div>
-            )
-          )}
+          <div className="flex flex-col space-y-4 lg:w-[75%] lg:mx-auto mt-16">
+            {["Manager", "Accountant", "Human Resource", "Security"].map(
+              (item, index) => (
+                <div
+                  key={index}
+                  className="flex items-center rounded-[8px] border-[.75px] border-[#D6D6D6] shadow-sm py-2 text-xs text-center"
+                >
+                  <span className="w-[30%] border-r py-1 font-bold">
+                    {item}
+                  </span>
+                  <span className="w-[40%] flex justify-center items-center space-x-2 border-r py-1">
+                    <Image
+                      src="/svg/flag.svg"
+                      alt="photo flag"
+                      width={20}
+                      height={20}
+                      className="rounded-full "
+                    />
+                    <p>Lagos - Nigeria</p>
+                  </span>
+                  <span className="w-[30%] py-1">Full-time</span>
+                </div>
+              )
+            )}
+          </div>
         </div>
-      </div>
-    </MaxWidthWrapper>
+      </MaxWidthWrapper>
+    </div>
   );
 };
