926438e925624739e0410910a5d4bb47f2ad0b36
Tobi Adesokan <70437868+tobisamcode@users.noreply.github.com>
2024-12-31 00:35:29 +0100

    new fixes 🎉

---
+++ src/app/(landing)/current-account/components/savings-page.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import Image from "next/image";
+import { Hero } from "./saving-hero";
+import { DocumentRequired } from "./document-required";
+import { SavvingsBenefit } from "./benefit";
+
+export const CurrentAccountPage = () => {
+  return (
+    <>
+      <div className="relative">
+        {/* Hero section */}
+        <Hero />
+
+        {/* Optional: Background grid effect */}
+        <div className="absolute top-[40%] -z-20 opacity-70  left-0 right-0 w-full ">
+          <Image
+            src="/images/building.png"
+            alt="photo Building"
+            width={1000}
+            height={1000}
+            className="h-full w-full object-cover"
+          />
+        </div>
+      </div>
+
+      {/* Document Required */}
+      <div className="relative py-12 lg:py-24">
+        <div className="absolute inset-x-0 inset-y-0  bg-dark-primary -z-10" />
+        <DocumentRequired />
+      </div>
+
+      {/* Benefit of Savings */}
+      <SavvingsBenefit />
+    </>
+  );
+};
