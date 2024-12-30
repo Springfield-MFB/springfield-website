cdc72ab58915ad6d0421a7cd68383aa6dd3790b2
Tobi Adesokan <70437868+tobisamcode@users.noreply.github.com>
2024-12-31 00:34:39 +0100

    new fixes 🎉

--- src/app/(landing)/asset-acquisition-loan/page.tsx
+++ src/app/(landing)/asset-acquisition-loan/page.tsx
@@ -3,7 +3,7 @@ import { AssetAcquisitionLoan } from "./components/asset-acquisition-loan";
 export const metadata = {
   title: "Asset Acquisition Loan",
   description:
-    "This is a product designed to give our customers access to funding asset acquisition like Home appliances, electronics and phones etc.",
+    "This is a product designed to give our customers access to funding asset acquisition like Home appliances, electronics, cars and phones etc.",
 };
 
 const Page = () => {
