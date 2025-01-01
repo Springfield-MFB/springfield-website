cefbaa70bb710c5d054d773c878156cfd6667a5e
Tobi Adesokan <70437868+tobisamcode@users.noreply.github.com>
2025-01-01 23:45:31 +0100

    new fixes 🎉

--- src/app/(landing)/shelter-loan/components/housing-loan-page.tsx
+++ src/app/(landing)/shelter-loan/components/housing-loan-page.tsx
@@ -9,10 +9,8 @@ export const HousingLoanPage = () => {
   return (
     <>
       <div className="relative ">
-        {/* Hero section */}
         <LoanHero />
 
-        {/* Optional: Background grid effect */}
         <div className="absolute top-[33%] -z-20 opacity-70  left-0 right-0 w-full ">
           <Image
             src="/images/building.png"
@@ -24,10 +22,8 @@ export const HousingLoanPage = () => {
         </div>
       </div>
 
-      {/* Benefit of Loan */}
       <LoanBenefit />
 
-      {/* Loan Calculator */}
       <div className="relative py-12">
         <div className="absolute inset-x-0 inset-y-0  bg-dark-primary -z-10" />
         <LoanCalculator mode="dark" />
