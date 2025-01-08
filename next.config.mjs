9ad5a296347bdb910fae62adfecd2f616452a2fe
Tobi Adesokan <70437868+tobisamcode@users.noreply.github.com>
2025-01-08 13:00:54 +0100

    final new fixes 🎉

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true, // Skips ESLint checks during the build
   },
+  webpack: (config) => {
+    config.resolve.alias["my-alias"] = "some/path"; // Correct path
+    return config;
+  },
 };
 
 export default nextConfig;
