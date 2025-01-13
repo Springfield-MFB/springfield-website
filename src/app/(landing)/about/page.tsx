671217d36fe483404becd0c965ef071b13ace320
Tobi Adesokan <70437868+tobisamcode@users.noreply.github.com>
2025-01-13 15:07:29 +0100

    final new fixes 🎉

--- src/app/(landing)/about/page.tsx
+++ src/app/(landing)/about/page.tsx
@@ -3,7 +3,7 @@ import { AboutPage } from "./comoponents/about-page";
 export const metadata = {
   title: "About us",
   description:
-    "Springfield Micro Finance Bank Limited (SMFB), situated in Igando, Lagos State, is a licensed Microfinance Bank dedicated to providing cutting-edge microfinance services/products to underserved communities and MSME through technological innovation.",
+    "Springfield Micro Finance Bank Limited (SMFB), situated at Igando, Lagos State, is a licensed Microfinance Bank dedicated to providing cutting-edge microfinance services/products to underserved communities and MSME through technological innovation.",
 };
 
 const Page = () => {
