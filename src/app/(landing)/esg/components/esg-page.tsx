59efd8c45287936d804be3d7a87491b04454a743
Tobi Adesokan <70437868+tobisamcode@users.noreply.github.com>
2025-02-10 23:42:22 +0100

    little fixes 👨🏽‍🔧

--- src/app/(landing)/esg/components/esg-page.tsx
+++ src/app/(landing)/esg/components/esg-page.tsx
@@ -7,7 +7,7 @@ export const EsgPage = () => {
     <div className="bg-[#F9F9F9]">
       <PageBoard
         containerClassName="lg:w-[90%]"
-        className="h-[80vh] lg:h-[90vh]"
+        className="h-[65vh] lg:h-[50vh]"
         backgroundImage="url('/images/esg-page-bg.png')"
         desc="At Springfield Microfinance Bank we are committed to driving sustainable growth through the integration of Environmental, Social, and Governance (ESG) principles into our operations. We believe that aligning our business goals with sustainability and ethical practices not only creates long-term value for our stakeholders but also contributes to a better world for future generations."
         heading="Environmental, Social, and Governance (ESG) at Springfield Microfinance Bank Limited"
